a3a27548d7b9dcf3a19e3020c4dfe46dd962d572
sarthakghuge18 <ghugesarthak7@gmail.com>
2024-04-22 22:22:14 +0530

    tree

--- tree/binary_tree.cpp
+++ tree/binary_tree.cpp
@@ -189,6 +189,74 @@ bool isbalanced(node *root){
     }
 }
 
+//****************************** boundry traversal *********************************
+
+    void traversalLeft(Node* root, vector<int> &ans){
+        
+        if((root == NULL)  || (root->left == NULL && root->right == NULL)){
+            return ;
+        }
+        
+        ans.push_back(root->data);
+        
+        if(root->left){
+            traversalLeft(root->left,ans);
+        }else{
+            traversalLeft(root->right,ans);
+        }
+    }
+    
+    
+    
+    void traversalLeaf(Node* root , vector<int> &ans){
+        
+        if(root == NULL){
+            return ;
+        }
+        
+        if(root->left == NULL && root->right == NULL){
+            ans.push_back(root->data);
+        }
+        
+        traversalLeaf(root->left,ans);
+        traversalLeaf(root->right,ans);
+    }
+    
+    
+    void traversalRight(Node* root, vector<int> &ans){
+        
+        if((root == NULL) || (root->left == NULL && root->right == NULL)){
+            return ;
+        }
+        
+        if(root->right){
+            traversalRight(root->right,ans);
+        }else{
+            traversalRight(root->left,ans);
+        }
+        
+        ans.push_back(root->data);
+    }
+
+    vector <int> boundary(Node *root)
+    {
+       vector<int>ans;
+       if(root==NULL){
+           return ans;
+       }
+       ans.push_back(root->data);
+       
+       traversalLeft(root->left,ans);
+       
+       traversalLeaf(root->left,ans);
+       traversalLeaf(root->right,ans);
+       
+       traversalRight(root->right,ans);
+       
+       return ans;
+       
+    }
+
 //****************************** main function *************************************
 
 int main()
